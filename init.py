ee47df41a19bd43aaf6ca5924949250859508515
orlnode <orlandodefi6@gmail.com>
2023-09-21 03:24:08 +0200

    implemantation de base

--- init.py
+++ init.py
@@ -1,7 +1,8 @@
 current_directory = os.getcwd()
 
 separator = os.path.sep
-DATABASE = "blockchain.db"
+DATABASE = "{current_directory}{separator}blockchain.db"
+
 attach(f"{current_directory}{separator}tools.py")
 attach(f"{current_directory}{separator}database.py")
 attach(f"{current_directory}{separator}blockchain.py")
